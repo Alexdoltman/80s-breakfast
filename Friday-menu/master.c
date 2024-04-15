2776d20a0ac57fe90c734758868ad1208bd877db
Aleksey Dolotov <alexkerjonov@gmail.com>
2024-04-15 13:58:05 +0300

    Nice day/

--- Friday-menu/master.c
+++ Friday-menu/master.c
@@ -10,24 +10,5 @@ int main()
 {
     char *locale = setlocale(LC_ALL, "En-GB");
 
-          uint32_t Number = {0UL}, type;
-
-     float Array[5UL];
-
-     while(Number < 5UL)
-     {
-        scanf("%lu", &Array[type]);
-
-        ++Number;
-     }
-     for(uint32_t j = 0UL; j < Number; ++j)
-     {
-        if(Array[j] < 5UL)
-        {
-            printf("The index, [%lu] and its number: %lu\n", j, Array[j]);
-        }
-        printf("Output: %lu - [%lu];\n", j, Array[j]);
-     }
-
-    return 0ULL;
+          return 0ULL;
 }
