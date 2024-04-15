578a14375a9b42881e0c5bc2382c193598717b53
Aleksey Dolotov <alexkerjonov@gmail.com>
2024-04-15 12:19:36 +0300

    Cool

--- Friday-menu/master.c
+++ Friday-menu/master.c
@@ -4,8 +4,28 @@
 
 #include <stdint.h>
 
+#include <locale.h>
+
 int main()
 {
-    
+    char *locale = setlocale(LC_ALL, "En-GB");
+
+    unsigned long int num, nLine = 0Ul, SumOfn;
+
+    printf("Input a digit: \n");
+
+    scanf("%lu", &num);
+
+    for(unsigned long int n = 0UL; n < num - 1UL; ++n)
+    {
+        for(nLine = 1UL; nLine < num - 1UL; ++nLine)
+        {
+            SumOfn += ((2UL * (n + SumOfn)) - 1UL);
+
+            printf("Output: %lu;\n", SumOfn);
+        }
+        printf("\n");
+    }
+
     return 0ULL;
 }
