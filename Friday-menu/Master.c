a36ff3d208d7848929dd1afc93768afc88b90d77
Aleksey Dolotov <alexkerjonov@gmail.com>
2024-05-04 15:21:03 +0300

    Fix 0.0

--- Friday-menu/Master.c
+++ Friday-menu/Master.c
@@ -12,32 +12,9 @@ int main()
 {
     char *locale = setlocale(LC_ALL, "En-GB");
 
-    uint32_t digit;
+    const float Pi = 3.1415926535f; // Это число P.
 
-    printf("Input a digit: \n"); 
+    printf("Output: %0.001f\n", Pi);
 
-    scanf("%lu", &digit);
-
-    for(uint32_t m = 0UL; m < digit - 2UL; ++m)
-    {
-        for(uint32_t k = 0UL; k <= ((digit - m) / 2UL); ++k)
-        {
-            printf("*");
-        }
-        printf("\n");
-    }
-
-    printf("\n-----------------------------\n");
-
-    for(uint32_t m = digit; m > 0UL; --m){
-        for(uint32_t n = digit; 2U * n > 0UL; --n){
-            printf("*");
-        }
-        printf("\n");
-    }
-
-    
-
-   
      return 0ULL;
 }
