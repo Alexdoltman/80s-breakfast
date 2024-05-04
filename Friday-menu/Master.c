44423c0a8329272481b2970aca9724afb2e27a97
Aleksey Dolotov <alexkerjonov@gmail.com>
2024-05-04 15:26:04 +0300

    I have got to hard work!

--- Friday-menu/Master.c
+++ Friday-menu/Master.c
@@ -12,9 +12,77 @@ int main()
 {
     char *locale = setlocale(LC_ALL, "En-GB");
 
-    const float Pi = 3.1415926535f; // Это число P.
+    /*
+        Задача: Дано целое число k (1 ≤ k ≤ 365). Присвоить целочисленной величине n значение 1, 2, ..., 6 или 0 в зависимости от того, на какой день недели (понедельник, 
+ 
+12 Глава 2
+вторник, ..., субботу или воскресенье) приходится k-й день года, в котором 
+1 января:
+а) понедельник;
+б) вторник;
+в)* d-й день недели (если 1 января— понедельник, то 
+d 1
+, если вторник —
+d 2 , ..., если воскресенье — d 7
+).
+    */
 
-    printf("Output: %0.001f\n", Pi);
+   uint16_t n, // от французского слова numéro, то есть номер
+            k; // это целое число.
+    
+    printf("Input a digit: ");
+
+    scanf("%hu", &k);
+
+    if(k >= 1U && k <= 365U)
+    {
+        n += k % 7U;
+
+        if((k % 7U) >= 1U && (k % 7U) <= 7U)
+        {
+           switch(n)
+           {
+            case 1U:
+                printf("The Monday!\n And its number is %hu\n", n);
+                return 0U;
+                break;
+
+                case 2U:
+                printf("The Tuesday!\nAnd its number is %hu\n",n);
+                break;
+
+                case 3U:
+                printf("The Wednesday!\nAnd it's number is %hu\n",n);
+                break;
+
+                case 4U:
+                printf("The Thirsday!\nAnd it's number is %hu\n",n);
+                break;
+
+                case 5U:
+                printf("The Friday\nAnd it's number is %hu\n",n);
+                break;
+
+                case 6U:
+                printf("The Saturday\nAnd it's number is %hu\n",n);
+                break;
+
+                case 7U:
+                printf("The Sunday\nAnd it's number is %hu\n", n);
+                break;
+
+                default:
+                return - 1l;
+                break;
+           }
+        }
+
+        else{
+            perror("You have got an error!\n");
+            return -1;
+        }
+
+    }
 
      return 0ULL;
 }
